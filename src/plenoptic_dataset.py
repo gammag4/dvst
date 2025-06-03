b5e490560aac13a0b176620dad617ca43cfb6e0c
Gabriel Maia Gondim <41156120+gammag4@users.noreply.github.com>
2025-06-03 05:58:07 +0000

    Organize PlenopticDataset code

--- src/plenoptic_dataset.py
+++ src/plenoptic_dataset.py
@@ -31,36 +31,31 @@ class PlenopticDataset(Dataset):
         self.fps = 30
         self.is_test = is_test
         scene_names = list(filter(lambda p: os.path.isdir(os.path.join(path, p)), os.listdir(path)))
-        self.data = [self.load_data(n) for n in scene_names]
+        
+        scenes = []
+        for sname in scene_names:
+            spath = os.path.join(self.path, sname)
+            vids = sorted(filter(lambda n: not n.endswith('.npy'), os.listdir(spath)))
+            vids = [os.path.join(spath, v) for v in vids]
 
-    def load_data(self, scene_name):
-        spath = os.path.join(self.path, scene_name)
-        vids = sorted(filter(lambda n: not n.endswith('.npy'), os.listdir(spath)))
-        vids = [os.path.join(spath, v) for v in vids]
+            poses_raw = torch.from_numpy(np.load(os.path.join(spath, 'poses_bounds.npy')))
+            K, T, _ = colmap_poses_to_intrinsics_extrinsics(poses_raw)
+            R, t = T[:, :, :3], T[:, :, 3:]
+            K, R, t = [list(i) for i in [K, R, t]]
+            
+            fps = [self.fps] * len(vids)
 
-        poses_raw = torch.from_numpy(np.load(os.path.join(spath, 'poses_bounds.npy')))
-        K, T, _ = colmap_poses_to_intrinsics_extrinsics(poses_raw)
-        R, t = T[:, :, :3], T[:, :, 3:]
-        K, R, t = [list(i) for i in [K, R, t]]
+            res = list(zip(vids, K, R, t, fps))
+            # First video is the test one
+            res = res[:1] if self.is_test else res[1:]
 
-        res = list(zip(vids, K, R, t))
-        # First video is the test one
-        res = res[:1] if self.is_test else res[1:]
+            scenes.append(res)
 
-        return res
+        self.data = scenes
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, i):
         d = self.data[i]
-
-        return [
-            preprocess_scene_video(
-                v[0],
-                v[1],
-                v[2],
-                v[3],
-                self.fps
-            ) for v in d
-        ]
+        return [preprocess_scene_video(*v) for v in d]
