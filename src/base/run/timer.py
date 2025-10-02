783d162c8691061300356a03833db3df4cf4c3a6
Gabriel Maia Gondim <41156120+gammag4@users.noreply.github.com>
2025-10-02 04:13:02 +0000

    Update timer step counting logic

--- src/base/run/timer.py
+++ src/base/run/timer.py
@@ -2,29 +2,35 @@ import time
 
 
 class Timer:
-    def __init__(self):
+    def __init__(self, total_steps = None):
         self._n_deltas = 0
         self.delta = 0.0
         self.avg_delta = 0.0
         self._last_time = time.perf_counter()
+        self.total_steps = total_steps
     
-    def eta(self, total_n):
-        return total_n * self.avg_delta
+    @property
+    def eta(self):
+        if self.total_steps is None:
+            return None
+        
+        return self.total_steps * self.avg_delta
     
-    def update(self):
+    def update(self, increment_size=1):
         t = time.perf_counter()
         self.delta = t - self._last_time
         self._last_time = t
-        self.avg_delta = (self._n_deltas * self.avg_delta + self.delta) / (self._n_deltas + 1)
+        self.avg_delta = (self._n_deltas * self.avg_delta + increment_size * self.delta) / (self._n_deltas + increment_size)
         
-        self._n_deltas += 1
+        self._n_deltas += increment_size
     
     def state_dict(self):
         return {
             'n_deltas': self._n_deltas,
             'delta': self.delta,
             'avg_delta': self.avg_delta,
-            'last_time': self._last_time
+            'last_time': self._last_time,
+            'total_steps': self.total_steps,
         }
     
     def load_state_dict(self, state_dict):
@@ -32,3 +38,4 @@ class Timer:
         self.delta = state_dict['delta']
         self.avg_delta = state_dict['avg_delta']
         self._last_time = state_dict['last_time']
+        self.total_steps = state_dict['total_steps']
