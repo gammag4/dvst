d06350f200371229c9b8b2562f91dcda8df0483c
Gabriel Maia Gondim <41156120+gammag4@users.noreply.github.com>
2025-10-07 07:20:11 +0000

    Fix perceptual loss cropping original image before computing loss

--- src/dvst/loss/loss.py
+++ src/dvst/loss/loss.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 from torchvision.models import convnext_tiny, ConvNeXt_Tiny_Weights
+import torchvision.transforms.functional as F
 
 
 class PerceptualLoss(nn.Module):
@@ -11,30 +12,37 @@ class PerceptualLoss(nn.Module):
         # TODO add layer_weights scheduler
         weights = ConvNeXt_Tiny_Weights.DEFAULT
         model = convnext_tiny(weights=weights)
-
+        
         model.eval()
         for param in model.parameters():
             param.requires_grad = False
-
+        
         self.transforms = weights.transforms()
+        self.transforms.resize_size=224
         self.layers = [lambda x: x] + list(model.features) + [lambda x: model.classifier(model.avgpool(x))]
         self.model = model
         
         self.layer_weights = layer_weights
-
-    def forward(self, input, target):
-        x1, x2 = self.transforms(input), self.transforms(target)
+    
+    def forward_layer(self, x1, x2):
         offset = len(x1.shape) - 3
+        
+        # C * H * W
+        N = x1.shape[offset:].numel()
+        return torch.norm(x1 - x2, p=2, dim=-1).sum() / N
+    
+    def forward(self, input, target):
+        x1 = F.center_crop(input, max(input.shape[-1], input.shape[-2]))
+        x2 = F.center_crop(target, max(target.shape[-1], target.shape[-2]))
+        x1, x2 = self.transforms(x1), self.transforms(x2)
+        
         losses = []
-
         for l in self.layers:
             x1, x2 = l(x1), l(x2)
-            
-            # C * H * W
-            N = x1.shape[offset:].numel()
-            losses.append(torch.norm(x1 - x2, p=2, dim=-1).sum() / N)
-
+            losses.append(self.forward_layer(x1, x2))
+        
         weights = self.layer_weights.to(input.device)
         weights = weights / weights.sum() # Normalizes weights
+        
         loss = (torch.stack(losses) * weights).sum()
         return loss
