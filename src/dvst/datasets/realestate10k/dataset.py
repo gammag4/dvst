8eb0d232af835fd2e33b28bc145e6fdaa6471096
Gabriel Maia Gondim <41156120+gammag4@users.noreply.github.com>
2025-11-06 23:18:06 +0000

    Make RE10k dataset iterable

--- src/dvst/datasets/realestate10k/dataset.py
+++ src/dvst/datasets/realestate10k/dataset.py
@@ -1,28 +1,26 @@
 import os
+import random
 import math
 import torch
 from torchvision.io import decode_image
 
 from src.base.utils import json_load
 
-from src.dvst.datasets.scene_dataset import StaticScene, SceneData, IndexableSceneDataset, process_data
+from src.dvst.datasets.scene_dataset import StaticScene, SceneData, IterableSceneDataset, process_data
 
 
 class PixelSplatRE10KSceneData(SceneData):
-    def __init__(self, scene_data, path, n_sources, n_target_frames, resize_to):
-        self.scene_data = scene_data
-        self.path = path
+    def __init__(self, scene, n_sources, n_target_frames, resize_to):
+        self.scene = scene
         self.n_sources = n_sources
         self.n_target_frames = n_target_frames
         self.resize_to = resize_to
     
     def load(self, device):
-        scene = torch.load(os.path.join(self.path, self.scene_data.file))[self.scene_data.file_index]
-        
-        scene_name = scene['key']
-        # times = scene['timestamps'] / 1000000 # From microseconds to seconds
-        I = torch.stack([decode_image(i) for i in scene['images']]).to(device, non_blocking=True)
-        cameras = scene['cameras'].to(device, non_blocking=True)
+        scene_name = self.scene['key']
+        # times = self.scene['timestamps'] / 1000000 # From microseconds to seconds
+        I = torch.stack([decode_image(i) for i in self.scene['images']]).to(device, non_blocking=True)
+        cameras = self.scene['cameras'].to(device, non_blocking=True)
         
         fx, fy, px, py = [cameras[:, i] for i in range(4)]
         K = torch.zeros((fx.shape[0], 3, 3), device=device)
@@ -53,7 +51,7 @@ class PixelSplatRE10KSceneData(SceneData):
         )
 
 
-class PixelSplatRealEstate10KDataset(IndexableSceneDataset):
+class PixelSplatRealEstate10KDataset(IterableSceneDataset):
     def __init__(self, path, resize_to: tuple[int, int] | None, n_sources: int, n_target_frames: int):
         super().__init__()
         self.path = path
@@ -62,12 +60,20 @@ class PixelSplatRealEstate10KDataset(IndexableSceneDataset):
         self.n_target_frames = n_target_frames
         
         self.scenes = json_load(os.path.join(self.path, 'data.json')).scenes
+        self.batches = [i for i in os.listdir(self.path) if i.endswith('.torch')]
+        random.shuffle(self.batches)
+        self.current_batch = 0
+        self.current_index = 0
     
     def __len__(self):
         return len(self.scenes)
     
-    def __getitem__(self, i):
-        return PixelSplatRE10KSceneData(self.scenes[i], self.path, self.n_sources, self.n_target_frames, self.resize_to)
+    def __iter__(self):
+        for b in self.batches:
+            batch = torch.load(os.path.join(self.path, b))
+            random.shuffle(batch)
+            for s in batch:
+                yield PixelSplatRE10KSceneData(s, self.n_sources, self.n_target_frames, self.resize_to)
     
     @property
     def n_frames(self):
@@ -79,39 +85,3 @@ class PixelSplatRealEstate10KDataset(IndexableSceneDataset):
     
     def get_n_batches(self, batch_size):
         return sum([math.ceil(s.n_frames / batch_size) for s in self.scenes])
-
-
-# class BatchedDataset(IndexableSceneDataset):
-#     def __init__(self, dataset: IndexableSceneDataset, batch_size):
-#         super().__init__()
-#         self.dataset = dataset
-#         self.batch_size = batch_size
-    
-#     def __len__(self):
-#         return self.get_n_batches(self.batch_size)
-    
-#     def __getitem__(self, i):
-#         scenes = [self.dataset[j] for j in range(i * self.batch_size, min(len(self.dataset), (i + 1) * self.batch_size))]
-        
-#         sources, targets = zip(*[[s.sources, s.targets] for s in scenes])
-#         sources, targets = [zip(*[[s.K, s.R, s.t, s.time, s.I] for s in ss]) for ss in (sources, targets)]
-#         sources, targets = [SourceBatch(*[torch.concat(i) for i in ss]) for ss in (sources, targets)]
-        
-#         return edict(
-#             sources=sources,
-#             targets=targets,
-#             scene_ids=[sid for s in scenes for sid in s.scene_ids],
-#             start=0,
-#             end=len(scenes)
-#         )
-    
-#     @property
-#     def n_frames(self):
-#         return self.dataset.n_frames
-    
-#     @property
-#     def n_scenes(self):
-#         return self.dataset.n_scenes
-    
-#     def get_n_batches(self, batch_size):
-#         return self.dataset.get_n_batches(batch_size)
